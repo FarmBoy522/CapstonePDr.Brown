5a28691daad79244d20e6b2fd19a767be5214360
Kyle Fox <kylefox123@gmail.com>
2024-04-09 14:08:13 -0400

    Update main.py

--- KyleFox-Folder/PUD/main.py
+++ KyleFox-Folder/PUD/main.py
@@ -57,32 +57,57 @@ sm.active(1)
 
 
 def ControlStateMachine(PinArray):
-    state = 0
     flag = 1
+    global state
+    
+    #Input Control Pins
+    strt = DebouncedInput(20, startcall, pin_pull=Pin.PULL_DOWN)
+    stop = DebouncedInput(19, stopcall, pin_pull=Pin.PULL_DOWN)
+    slct = DebouncedInput(18, selectcall, pin_pull=Pin.PULL_DOWN)
     
     while flag == 1:
-        # STATE 0 - WAIT FOR BUTTON PRESS
+        # STATE 0 - WAIT FOR BUTTON PRESS - This is the stop state
         if state == 0:
-            while state == 0:
-                if debounce(PinArray[16]) == 1:		#Start Button Pushed
-                    print("Button pushed")
-                    state = 1
-                elif PinArray[17] == 1:		#Stop Button Pushed
-                    state = 0
-                elif PinArray[18] == 1:		#Select Button Pushed
-                    state = 2
+            print(state)
+            time.sleep(2)
+                    
         # STATE 1 - RUN PROGRAM
         elif state == 1:
-            state1(timeset, piosm, PinArray)
+            print(state)
+            #programs.state1(timeset, piosm, PinArray)
+            state = 0
+            
         # STATE 2 - CHANGE TOTAL TEST TIME
         elif state == 2:
-            sec, currentset = state2(currentset, flag)
+            print(state)
+            #sec, currentset = programs.state2(currentset, flag)
+            state = 0
+            
         # ERROR STATE - SOMETHING HAS GONE WRONG AND EXIT
         else:
+            print(state)
             print("Error: Wrong State")
-            flag == 0
+            flag = 0
             exit
+            
+            
     print("Crashed - Check for Errors")
+    
+def startcall(pressed):
+    global state
+    if pressed == True:
+        state = 1
+        
+def stopcall(pressed):
+    global state
+    if pressed:
+        state = 0
+        
+
+def selectcall(pressed):
+    global state
+    if pressed:
+        state = 2
 
 
 def setandinit():
@@ -104,46 +129,12 @@ def setandinit():
     voltage1 = ADC(Pin(26))
     voltage2 = ADC(Pin(27))
     
-    #Pulse Control Pins
-    ctrl1 = Pin(14, mode=Pin.OUT, pull=None, value=0)
-    ctrl2 = Pin(15, mode=Pin.OUT, pull=None, value=0)
-    
-    #Input Control Pins
-#     strt = DebouncedInput(20, callback, pin_pull=Pin.PULL_DOWN)
-#     stop = DebouncedInput(19, callback, pin_pull=Pin.PULL_DOWN)
-#     slct = DebouncedInput(18, callback, pin_pull=Pin.PULL_DOWN)
-    
-    strt = Pin(20, mode=Pin.IN, pull=Pin.PULL_DOWN)
-    stop = Pin(19, mode=Pin.IN, pull=Pin.PULL_DOWN)
-    slct = Pin(18, mode=Pin.IN, pull=Pin.PULL_DOWN)
-    
-    
     #Array of Pin Objects
-    #Array Num	0	1	2	3	4	5	6	7	8	9	10	 11		12		 13			14	   15	 16    17	 18
-    PinArray = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, voltage1, voltage2, ctrl1, ctrl2, strt, stop, slct]
-    #Pin Val	2	3	4	5	6	7	8	9	10	11	12	 13		26		 27			14	   15	 20	   19	 18
+    #Array Num	0	1	2	3	4	5	6	7	8	9	10	 11		12		 13
+    PinArray = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, voltage1, voltage2]
+    #Pin Val	2	3	4	5	6	7	8	9	10	11	12	 13		26		 27
     
     return PinArray
-
-def callback(pin, pressed, duration_ms):
-    if (pressed):
-        print("Pin-", pin, " Pressed:", duration_ms, "ms since last press")
-        return 1
-    else:
-        print("Pin-", pin, " Released:", duration_ms, "ms long press")
-        return 0
-        
-def debounce(pin):
-    # wait for pin to change value
-    # it needs to be stable for a continuous 20ms
-    cur_value = pin.value()
-    active = 0
-    while active < 20:
-        if pin.value() != cur_value:
-            active += 1
-        else:
-            active = 0
-        time.sleep_ms(1)
 
 def state1(timeset, piosm, PinArray):
     smpulse = rp2.StateMachine(0, pulse, freq=500000, set_base=machine.Pin(14))
@@ -173,5 +164,8 @@ def state2(currentset):
         return testcounter, currentset
 
 if __name__ == '__main__':
+    global state
+    state = 0
+    
     PinArray = setandinit()
     ControlStateMachine(PinArray)
